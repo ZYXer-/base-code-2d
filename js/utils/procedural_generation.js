361cf4e6b2eb1825f194de2ed41ad7f0a259aaab
Henry Raymond <github@henryraymond.com>
2016-09-29 14:19:24 +0200

    Fixed utils bugs in ProceduralGeneration

--- js/utils/procedural_generation.js
+++ js/utils/procedural_generation.js
@@ -10,16 +10,16 @@ ProceduralGeneration.perlinNoise = function(size, lowestOctave, highOctaveWeight
     var y;
 
     for(var octaveSize = size; octaveSize >= lowestOctave; octaveSize /= 2) {
-        octave = createMatrix(octaveSize, octaveSize, 0);
+        octave = Utils.createMatrix(octaveSize, octaveSize, 0);
         for(x = 0; x < octaveSize; x++) {
             for(y = 0; y < octaveSize; y++) {
-                octave[x][y] = randFloat(0.0, 1.0);
+                octave[x][y] = Utils.randFloat(0.0, 1.0);
             }
         }
         octaves.push(octave);
     }
 
-    var noise = createMatrix(size, size, 0);
+    var noise = Utils.createMatrix(size, size, 0);
 
     for(var i = 0; i < octaves.length; i++) {
 
@@ -41,8 +41,8 @@ ProceduralGeneration.perlinNoise = function(size, lowestOctave, highOctaveWeight
                 var x0y1 = octave[baseX][(baseY + 1) % oS];
                 var x1y1 = octave[(baseX + 1) % oS][(baseY + 1) % oS];
 
-                var interpolateX = limit(Interpolate.quad(offsetX), 0.0, 1.0);
-                var interpolateY = limit(Interpolate.quad(offsetY), 0.0, 1.0);
+                var interpolateX = Utils.limit(Interpolate.quad(offsetX), 0.0, 1.0);
+                var interpolateY = Utils.limit(Interpolate.quad(offsetY), 0.0, 1.0);
 
                 var value = x0y0 * (1.0 - interpolateX) * (1.0 - interpolateY);
                 value += x1y0 * interpolateX * (1.0 - interpolateY);
@@ -87,7 +87,7 @@ ProceduralGeneration.scaleMatrixToMinMax = function(m, min, max) {
 
     for(x = 0; x < sizeX; x++) {
         for(y = 0; y < sizeY; y++) {
-            m[x][y] = limit(min + halfSpan + ((m[x][y] - mid) * scale * span), min, max);
+            m[x][y] = Utils.limit(min + halfSpan + ((m[x][y] - mid) * scale * span), min, max);
         }
     }
 
@@ -96,21 +96,21 @@ ProceduralGeneration.scaleMatrixToMinMax = function(m, min, max) {
 
 ProceduralGeneration.createIsland = function(map, mapSizeX, mapSizeY, posX, posY, radius, amplitude, callback) {
 
-    var startX = limit(posX - (radius + amplitude), 0, mapSizeX - 1);
-    var endX = limit(posX + (radius + amplitude), 0, mapSizeX - 1);
-    var startY = limit(posY - (radius + amplitude), 0, mapSizeY - 1);
-    var endY = limit(posY + (radius + amplitude), 0, mapSizeY - 1);
+    var startX = Utils.limit(posX - (radius + amplitude), 0, mapSizeX - 1);
+    var endX = Utils.limit(posX + (radius + amplitude), 0, mapSizeX - 1);
+    var startY = Utils.limit(posY - (radius + amplitude), 0, mapSizeY - 1);
+    var endY = Utils.limit(posY + (radius + amplitude), 0, mapSizeY - 1);
 
     var possibleFrequencies = [3, 5, 7, 9, 11];
     var frequency = possibleFrequencies[rand(0, Math.ceil(radius / 3.0))];
-    var offset = randFloat(0.1, 2.5);
+    var offset = Utils.randFloat(0.1, 2.5);
 
     for(var x = startX; x <= endX; x++) {
         for(var y = startY; y <= endY; y++) {
             var angle1 = offset + (Math.atan2(posX - x, posY - y) * frequency);
             var angle2 = (offset * 3) + (Math.atan2(posX - x, posY - y) * 3);
             var maxDist = radius + (0.5 * amplitude * Math.sin(angle1)) + (0.5 * amplitude * Math.sin(angle2));
-            if(distance(x, y, posX, posY) < maxDist) {
+            if(Utils.distance(x, y, posX, posY) < maxDist) {
                 callback(map, x, y);
             }
         }
